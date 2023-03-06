16e9d99e966a807a760fb736a51da56b4828dead
Pedro Ricardo <ordepricardo13@gmail.com>
2023-03-06 13:02:18 -0300

    Módulo 1 > Aula 8

---
+++ modulo-1/aula-8/script.js
@@ -0,0 +1,35 @@
+// Data types
+// Number
+// String
+// Boolean
+
+// Number
+console.log('Soma (1 + 1)', 1 + 1)
+
+console.log('Subtração', 5 - 1)
+
+console.log('Multiplicação', 2 * 3)
+
+console.log('Divisão', 4 / 2)
+
+console.log('Outras operações', 4 + (2 * 2))
+
+// String
+console.log('Olá Mundo!')
+
+console.log('Olá ' + 'Mundo!')
+
+console.log('2' * '3')
+
+console.log('meu nome é pedro'.toUpperCase())
+
+console.log('MEU NOME É PEDRO')
+
+// Boolean
+console.log(true, false)
+
+console.log(3 < 4)
+
+console.log(4 === 4)
+
+console.log(3 >= 4)
