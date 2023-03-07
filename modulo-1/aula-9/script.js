c4c3dc69dbc28f0cbb29f0f8c69ff3bd0d6937f9
Pedro Ricardo <ordepricardo13@gmail.com>
2023-03-07 13:00:23 -0300

    Modulo 1 > Aula 9

---
+++ modulo-1/aula-9/script.js
@@ -0,0 +1,30 @@
+// Condicionais
+
+var comparacao = 1 < 0;
+var outraComparacao = 0 < 1;
+
+if (comparacao) {
+    console.log('verdadeiro');
+} else if (outraComparacao) {
+    console.log('outro verdadeiro');
+} else {
+    console.log('falso');
+}
+
+if (comparacao && outraComparacao) {
+    console.log('verdadeiro');
+} else {
+    console.log('falso');
+}
+
+if (comparacao || outraComparacao) {
+    console.log('verdadeiro');
+} else {
+    console.log('falso');
+}
+
+if (!comparacao) {
+    console.log('falso')
+}
+
+// < > <= >= == !=
